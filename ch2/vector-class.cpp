9aafaeb8987c1c06eea0391eccfbe7b42370f457
aminos <amri.amine@protonmail.com>
2019-06-08 16:09:16 +0100

    Created a read() and sum() functions - Using std::cin to read values

--- ch2/vector-class.cpp
+++ ch2/vector-class.cpp
@@ -14,12 +14,31 @@ class Vector {
     int sz;
 };
 
+void read(Vector& v)
+{
+  for (int i = 0; i != v.size(); i++)
+    cin >> v[i];
+}
+
+
+double sum(Vector& v)
+{
+  double s = 0;
+  for (int i = 0; i < v.size(); i++)
+    s += v[i];
+
+  return s;
+}
+
+
 
 int main()
 {
   Vector v(5);
   v[0] = 5;
   cout << v[0] << "\n";
+  read(v);
+  cout << sum(v) << "\n";
 
   return 0;
 }
